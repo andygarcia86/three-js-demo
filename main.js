cdbbb4349e89ce512246fbc5a5deccfd21e5c69c
Andy Garcia <andygarcia86@gmail.com>
2020-07-04 16:53:52 -0500

    Add Tween animation lib

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ function init() {
 	var gui = new dat.GUI();
 	var clock = new THREE.Clock();
 
-	var enableFog = false;
+	var enableFog = true;
 
 	if (enableFog) {
-		scene.fog = new THREE.FogExp2(0xffffff, 0.2);
+		scene.fog = new THREE.FogExp2(0xffffff, 0.01);
 	}
 	
-	var plane = getPlane(30);
+	var plane = getPlane(100);
 	var directionalLight = getDirectionalLight(1);
 	var sphere = getSphere(0.05);
-	var boxGrid = getBoxGrid(10, 1.5);
+	var boxGrid = getBoxGrid(20, 2.5);
 
 	plane.name = 'plane-1';
 	boxGrid.name = 'boxGrid';
@@ -35,18 +35,58 @@ function init() {
 		1000
 	);
 
+	var cameraZRotation = new THREE.Group();
+	var cameraYPosition = new THREE.Group();
 	var cameraZPosition = new THREE.Group();
 	var cameraXRotation = new THREE.Group();
 	var cameraYRotation = new THREE.Group();
 
-	cameraZPosition.add(camera);
+	cameraZRotation.name = 'cameraZRotation';
+	cameraYPosition.name = 'cameraYPosition';
+	cameraZPosition.name = 'cameraZPosition';
+	cameraXRotation.name = 'cameraXRotation';
+	cameraYRotation.name = 'cameraYRotation';
+
+	cameraZRotation.add(camera);
+	cameraYPosition.add(cameraZRotation);
+	cameraZPosition.add(cameraYPosition);
 	cameraXRotation.add(cameraZPosition);
 	cameraYRotation.add(cameraXRotation);
 	scene.add(cameraYRotation);
 
+	cameraXRotation.rotation.x = -Math.PI/2;
+	cameraYPosition.position.y = 1;
+	cameraZPosition.position.z = 100;
+
+	new TWEEN.Tween({val: 100})
+		.to({val: -50}, 12000)
+		.onUpdate(function() {
+			cameraZPosition.position.z = this.val;
+		})
+		.start();
+
+	new TWEEN.Tween({val: -Math.PI/2})
+		.to({val: 0}, 6000)
+		.delay(1000)
+		.easing(TWEEN.Easing.Quadratic.InOut)
+		.onUpdate(function() {
+			cameraXRotation.rotation.x = this.val;
+		})
+		.start();
+
+	new TWEEN.Tween({val: 0})
+		.to({val: Math.PI/2}, 6000)
+		.delay(1000)
+		.easing(TWEEN.Easing.Quadratic.InOut)
+		.onUpdate(function() {
+			cameraYRotation.rotation.y = this.val;
+		})
+		.start();
+
 	gui.add(cameraZPosition.position, 'z', 0, 100);
 	gui.add(cameraYRotation.rotation, 'y', -Math.PI, Math.PI);
 	gui.add(cameraXRotation.rotation, 'x', -Math.PI, Math.PI);
+	gui.add(cameraZRotation.rotation, 'z', -Math.PI, Math.PI);
 
 	var renderer = new THREE.WebGLRenderer();
 	renderer.shadowMap.enabled = true;
@@ -79,12 +119,12 @@ function getBoxGrid(amount, separationMultiplier) {
 	var group = new THREE.Group();
 
 	for (var i=0; i<amount; i++) {
-		var obj = getBox(1, 1, 1);
+		var obj = getBox(1, 3, 1);
 		obj.position.x = i * separationMultiplier;
 		obj.position.y = obj.geometry.parameters.height/2;
 		group.add(obj);
 		for (var j=1; j<amount; j++) {
-			var obj = getBox(1, 1, 1);
+			var obj = getBox(1, 3, 1);
 			obj.position.x = i * separationMultiplier;
 			obj.position.y = obj.geometry.parameters.height/2;
 			obj.position.z = j * separationMultiplier;
@@ -148,10 +188,13 @@ function getDirectionalLight(intensity) {
 	var light = new THREE.DirectionalLight(0xffffff, intensity);
 	light.castShadow = true;
 
-	light.shadow.camera.left = -10;
-	light.shadow.camera.bottom = -10;
-	light.shadow.camera.right = 10;
-	light.shadow.camera.top = 10;
+	light.shadow.camera.left = -40;
+	light.shadow.camera.bottom = -40;
+	light.shadow.camera.right = 40;
+	light.shadow.camera.top = 40;
+
+	light.shadow.mapSize.width = 4096;
+	light.shadow.mapSize.height = 4096;
 
 	return light;
 }
@@ -163,12 +206,16 @@ function update(renderer, scene, camera, controls, clock) {
 	);
 
 	controls.update();
+	TWEEN.update();
 
 	var timeElapsed = clock.getElapsedTime();
 
+	var cameraZRotation = scene.getObjectByName('cameraZRotation');
+	cameraZRotation.rotation.z = noise.simplex2(timeElapsed * 1.5, timeElapsed * 1.5) * 0.02;
+
 	var boxGrid = scene.getObjectByName('boxGrid');
 	boxGrid.children.forEach(function(child, index) {
-		var x = timeElapsed * 5 + index;
+		var x = timeElapsed + index;
 		child.scale.y = (noise.simplex2(x, x) + 1) / 2 + 0.001;
 		child.position.y = child.scale.y/2;
 	});
